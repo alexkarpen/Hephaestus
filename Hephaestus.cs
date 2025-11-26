9013519972be566f065ad65a50e464acd62cf2cd
Alexandros <alexkarpen@gmail.com>
2025-11-26 03:27:02 +0200

    2.0.2 version reversed

--- Hephaestus.cs
+++ Hephaestus.cs
@@ -26,7 +26,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Hephaestus";
     public override string Author { get; init; } = "alexkarpen";
     public override List<string>? Contributors { get; init; } = ["alexkarpen"];
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.3");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.3");
     
     
